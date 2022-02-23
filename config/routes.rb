cadb0ca5015cca101f64c4adf0f09ca617580a4c
hayanj <hayaaljuraysi@gmail.com>
2022-02-23 21:26:47 +0300

    Ready for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
   devise_for :users
-#To fix the get route error
-  devise_scope :user do
-     get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-  # devise_for :users do
-  #   get '/users/sign_out' => 'devise/sessions#destroy'
-  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'home#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
-  get 'my_friends', to: 'friendships#index'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
+  resources :users, only: [:show]
+
 end
