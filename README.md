1d33dfd26d0c33323002709096e5c882e64cfaa1
hayanj <hayaaljuraysi@gmail.com>
2022-02-23 11:28:53 +0300

    Add stock model and iex api key

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is the finance tracker app that was built using rails 6, devise, and bootstarp .
+This is the finance tracker app that was built using rails 6, devise, iex cloud API, and bootstarp .
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+#To fix the get route error
+  devise_scope :user do
+     get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  # devise_for :users do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
 end
