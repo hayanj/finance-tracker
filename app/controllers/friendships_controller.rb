a90d519dd2fbabbf0cb9a4473f9fcf60c4ca5e16
hayanj <hayaaljuraysi@gmail.com>
2022-02-23 19:48:54 +0300

    Add friends view with listing

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,7 @@
+class FriendshipsController < ApplicationController
+
+  def index
+    @friends = current_user.friends
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  get 'my_friends', to: 'friendships#index'
 end
