14bc8548e74d3a3dab56a27427e67d2b0b24adfa
hayanj <hayaaljuraysi@gmail.com>
2022-02-28 10:33:59 +0300

    Add refresh functionality to update prices, edited the home page to be more pretty

--- Gemfile
+++ Gemfile
@@ -24,7 +24,6 @@ gem 'devise'
 gem 'devise-bootstrap-views', '~> 1.0'
 gem 'iex-ruby-client'
 gem "font-awesome-rails"
-
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,5 @@
 class StocksController < ApplicationController
-  
+
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
@@ -19,5 +19,21 @@ class StocksController < ApplicationController
         format.js { render partial: 'users/result' }
       end
     end
+  end
+
+  def refresh
+    if params[:user_id].present?
+      user = User.find(params[:user_id])
+      puts "Refresh prices of #{user.full_name}"
+      user.stocks.each do |stock|
+        stock.refresh_price
+      end
+    else
+      puts "Refreshing all prices"
+      Stock.all.each do |stock|
+        stock.refresh_price
+      end
+    end
+    redirect_to my_portfolio_path
   end
 end
